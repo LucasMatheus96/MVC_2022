227a66e7b89c7f4e6e7b701186382ab64ec960e6
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2023-11-09 00:16:24 -0300

    Entity Framework - Configuração

--- MVC_2022/Program.cs
+++ MVC_2022/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_2022.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                 options.UseSqlServer(connection));
 
 var app = builder.Build();
 
